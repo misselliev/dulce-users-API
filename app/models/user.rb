0f59b8f0e70e15fb937b2a1e9c34d674789478db
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-07-03 12:40:07 -0700

    Adding validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  validates_presence_of :name, :lastname, :email, :username
 end
